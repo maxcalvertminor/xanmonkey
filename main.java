ae07463a87f2b9b8c0f1d0423233c231595b6126
Max Calvert-Minor <maxincalvertminor@gmail.com>
2026-03-09 08:06:00 -0500

    Insulting name variable

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String name = "Sarb";
         System.out.println(name);
+        System.out.println(name + " is a little weak wussy face");
     }
 }
